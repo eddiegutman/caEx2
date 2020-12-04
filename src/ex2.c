8850573dc2736f2249c5a44cbbb256f3c96d2453
Eddie Gutman <edigutman@gmail.com>
2020-12-04 23:33:14 +0200

    Fully finished 2 of 3 functions

--- src/ex2.c
+++ src/ex2.c
@@ -2,123 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 
-const unsigned char winNewLineLE[4] = {0x0d, 0x00, 0x0a, 0x00};
-const unsigned char winNewLineBE[4] = {0x00, 0x0d, 0x00, 0x0a};
-const unsigned char macNewLineLE[2] = {0x0d, 0x00};
-const unsigned char macNewLineBE[2] = {0x00, 0x0d};
-const unsigned char unixNewLineLE[2] = {0x0a, 0x00};
-const unsigned char unixNewLineBE[2] = {0x00, 0x0a};
+const unsigned char WIN_NEW_LINE_LE[4] = {0x0d, 0x00, 0x0a, 0x00};
+const unsigned char WIN_NEW_LINE_BE[4] = {0x00, 0x0d, 0x00, 0x0a};
+const unsigned char MAC_NEW_LINE_LE[2] = {0x0d, 0x00};
+const unsigned char MAC_NEW_LINE_BE[2] = {0x00, 0x0d};
+const unsigned char UNIX_NEW_LINE_LE[2] = {0x0a, 0x00};
+const unsigned char UNIX_NEW_LINE_BE[2] = {0x00, 0x0a};
+const int ONE_BYTE = 1;
+const int TWO_BYTES = 2;
+const int FOUR_BYTES = 4;
+const int TRUE = 1;
+const int FALSE = 0;
+const int FIRST_BYTE = 0;
+const int SECOND_BYTE = 1;
+const unsigned int BOM_FIRST_BYTE = 0xFE;
+const unsigned int BOM_SECOND_BYTE = 0xFF;
+const int FIRST_OPTION = 3;
+const int SECOND_OPTION = 5;
+const int THIRD_OPTION = 6;
 
 void fileCopy(char *src, char *dst) {
     FILE *srcFile = fopen(src, "rb");
     FILE *dstFile = fopen(dst, "wb");
-    unsigned char buffer[2];
+    unsigned char buffer[TWO_BYTES];
     if (srcFile == NULL || dstFile == NULL)
         return;
-    while (fread(buffer, sizeof(char), sizeof(buffer), srcFile)) {
-        fwrite(buffer, sizeof(char), sizeof(buffer), dstFile);
+    while (fread(buffer, ONE_BYTE, sizeof(buffer), srcFile)) {
+        fwrite(buffer, ONE_BYTE, sizeof(buffer), dstFile);
     }
     fclose(srcFile);
     fclose(dstFile);
 }
 
-int checkEndianness(FILE *src, FILE *dst) {
-    unsigned char bom[2];
-    fread(bom, sizeof(char), sizeof(bom), src);
-    fwrite(bom, sizeof(char), sizeof(bom), dst);
-    if (bom[0] == 0xff && bom[1] == 0xfe)
-        return 1;
+int isLittleEndianness(FILE *src, FILE *dst) {
+    unsigned char buffer[TWO_BYTES];
+    fread(buffer, ONE_BYTE, sizeof(buffer), src);
+    fwrite(buffer, ONE_BYTE, sizeof(buffer), dst);
+    if (buffer[FIRST_BYTE] == BOM_SECOND_BYTE && buffer[SECOND_BYTE] == BOM_FIRST_BYTE)
+        return TRUE;
     else
-        return 0;
-}
-
-void lineEncoding(unsigned char byteArray[4], const char *system, int isLittleEndianness) {
-
-    if (strcmp(system, "-unix") == 0) {
-        if (isLittleEndianness) {
-            byteArray[0] = 0x0a;
-            byteArray[1] = 0x00;
-        } else {
-            byteArray[0] = 0x00;
-            byteArray[1] = 0x0a;
-        }
-    }
-    if (strcmp(system, "-mac") == 0) {
-        if (isLittleEndianness) {
-            byteArray[0] = 0x0d;
-            byteArray[1] = 0x00;
-        } else {
-            byteArray[0] = 0x00;
-            byteArray[1] = 0x0d;
-        }
-    }
-    if (strcmp(system, "-win") == 0) {
-        if (isLittleEndianness) {
-            byteArray[0] = 0x0d;
-            byteArray[1] = 0x00;
-            byteArray[2] = 0x0a;
-            byteArray[3] = 0x00;
-        } else {
-            byteArray[0] = 0x00;
-            byteArray[1] = 0x0d;
-            byteArray[2] = 0x00;
-            byteArray[3] = 0x0a;
-        }
-
-    }
+        return FALSE;
 }
 
 const unsigned char *mapEncoding(const char *system, int isLittleEndianness) {
     if (strcmp(system, "-unix") == 0)
-        return (isLittleEndianness) ? unixNewLineLE : unixNewLineBE;
-    if (strcmp(system, "-mac") == 0)
-        return (isLittleEndianness) ? macNewLineLE : macNewLineBE;
-    if (strcmp(system, "-win") == 0)
-        return (isLittleEndianness) ? winNewLineLE : winNewLineBE;
+        return (isLittleEndianness) ? UNIX_NEW_LINE_LE : UNIX_NEW_LINE_BE;
+    else if (strcmp(system, "-mac") == 0)
+        return (isLittleEndianness) ? MAC_NEW_LINE_LE : MAC_NEW_LINE_BE;
+    else if (strcmp(system, "-win") == 0)
+        return (isLittleEndianness) ? WIN_NEW_LINE_LE : WIN_NEW_LINE_BE;
 }
 
 int areEqual(const unsigned char *encoding, const unsigned char *buffer) {
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < TWO_BYTES; i++) {
         if (encoding[i] != buffer[i])
-            return 0;
+            return FALSE;
     }
-    return 1;
+    return TRUE;
 }
 
 void systemConvert(const char *src, const char *dst, const char *systemOld, const char *systemNew) {
     FILE *srcFile = fopen(src, "rb");
     FILE *dstFile = fopen(dst, "wb");
-    int isLittleEndianness;
-    int writeSize = (strcmp(systemNew, "-win") == 0) ? 4 : 2;
-    unsigned char buffer[2];
-    //unsigned char oldLineEncoding[4];
-    //unsigned char newLineEncoding[4];
 
-    if (srcFile == NULL || dstFile == NULL)
+    if (srcFile == NULL || dstFile == NULL) {
+        fclose(srcFile);
+        fclose(dstFile);
         return;
+    }
 
-    isLittleEndianness = checkEndianness(srcFile, dstFile);
-    //lineEncoding(oldLineEncoding, systemOld, isLittleEndianness);
-    //lineEncoding(newLineEncoding, systemNew, isLittleEndianness);
-    const unsigned char *oldLineEncoding = mapEncoding(systemOld, isLittleEndianness);
-    const unsigned char *newLineEncoding = mapEncoding(systemNew, isLittleEndianness);
+    int writeSize = (strcmp(systemNew, "-win") == 0) ? FOUR_BYTES : TWO_BYTES;
+    unsigned char buffer[TWO_BYTES];
 
-    while (fread(buffer, sizeof(char), sizeof(buffer), srcFile)) {
+    int isLE = isLittleEndianness(srcFile, dstFile);
+    const unsigned char *oldLineEncoding = mapEncoding(systemOld, isLE);
+    const unsigned char *newLineEncoding = mapEncoding(systemNew, isLE);
+
+    while (fread(buffer, ONE_BYTE, sizeof(buffer), srcFile)) {
         if (areEqual(oldLineEncoding, buffer)) {
             if (strcmp(systemOld, "-win") == 0) {
-                if (!fread(buffer, sizeof(char), sizeof(buffer), srcFile))
+                if (!fread(buffer, ONE_BYTE, sizeof(buffer), srcFile))
                     break;
-                if (areEqual(oldLineEncoding + 2, buffer))
-                    fwrite(newLineEncoding, sizeof(char), writeSize, dstFile);
+                if (areEqual(oldLineEncoding + TWO_BYTES, buffer))
+                    fwrite(newLineEncoding, ONE_BYTE, writeSize, dstFile);
                 else {
-                    fwrite(oldLineEncoding, sizeof(char), writeSize, dstFile);
-                    fwrite(buffer, sizeof(char), writeSize, dstFile);
+                    fwrite(oldLineEncoding, ONE_BYTE, writeSize, dstFile);
+                    fwrite(buffer, ONE_BYTE, writeSize, dstFile);
                 }
             } else {
-                fwrite(newLineEncoding, sizeof(char), writeSize, dstFile);
+                fwrite(newLineEncoding, ONE_BYTE, writeSize, dstFile);
             }
         } else {
-            fwrite(buffer, sizeof(char), sizeof(buffer), dstFile);
+            fwrite(buffer, ONE_BYTE, sizeof(buffer), dstFile);
         }
     }
     fclose(srcFile);
@@ -131,11 +106,11 @@ void endiannessConvert(char *src, char *dst, char *systemOld, char *systemNew, c
 
 
 int main(int argc, char *argv[]) {
-    if (argc == 3)
+    if (argc == FIRST_OPTION)
         fileCopy(argv[1], argv[2]);
-    if (argc == 5)
+    if (argc == SECOND_OPTION)
         systemConvert(argv[1], argv[2], argv[3], argv[4]);
-    if (argc == 6)
+    if (argc == THIRD_OPTION)
         endiannessConvert(argv[1], argv[2], argv[3], argv[4], argv[5]);
     return 0;
 }
